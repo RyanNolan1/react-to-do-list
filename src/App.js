850aab9ae3a9def582eda88368cccb8627c61c28
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2024-02-28 22:19:20 +0000

    this will add the layout for the text and radio input areas to add a new task

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import moonIcon from './images/icon-moon.svg'
-
+import moonIcon from "./images/icon-moon.svg";
 
 export default function App() {
   return (
@@ -10,10 +9,22 @@ export default function App() {
 }
 
 function Header() {
-  return <header>
-    <div className="title-toggle-container">
-      <h1>T O D O</h1>
-      <img src={moonIcon} alt="Moon Icon" />
-    </div>
-  </header>
+  return (
+    <header>
+      <div className="title-toggle-container">
+        <h1>T O D O</h1>
+        <img src={moonIcon} alt="Moon Icon" />
+      </div>
+        <AddTask />
+    </header>
+  );
+}
+
+function AddTask() {
+  return (
+  <div className="add-task-container">
+      <input id="add-task-radio" type="radio" name="add-task-radio"></input>
+      <input id="add-task-text" type="text" name="add-task-text" placeholder="Create a new todo…"></input>
+  </div>
+  )
 }
