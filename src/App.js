54bcdc9673f7ba03a2b21e3eb6970a4c91f61936
Ryan <ryannolan1990@hotmail.com>
2024-03-01 10:13:15 +0000

    this will update the placeholder text on each task using props

--- src/App.js
+++ src/App.js
@@ -15,30 +15,41 @@ function Header() {
         <h1>T O D O</h1>
         <img src={moonIcon} alt="Moon Icon" />
       </div>
-        <AddTask />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
+      <AddTask />
+      <Task task="Task 1" />
+      <Task task="Task 2" />
+      <Task task="Task 3" />
+      <Task task="Task 4" />
+      <Task task="Task 5" />
+      <Task task="Task 6" />
     </header>
   );
 }
 
 function AddTask() {
   return (
-  <div className="add-task-container">
+    <div className="add-task-container">
       <input id="add-task-radio" type="radio" name="add-task-radio"></input>
-      <input id="add-task-text" type="text" name="add-task-text" placeholder="Create a new todo…"></input>
-  </div>
-  )
+      <input
+        id="add-task-text"
+        type="text"
+        name="add-task-text"
+        placeholder="Create a new todo…"
+      ></input>
+    </div>
+  );
 }
 
-function Task() {
+function Task(props) {
   return (
     <div className="task-container">
       <input id="task-radio" type="radio" name="task-radio"></input>
-      <input id="task-text" type="text" name="ask-text" placeholder="Create a new todo…"></input>
+      <input
+        id="task-text"
+        type="text"
+        name="ask-text"
+        placeholder={props.task}
+      ></input>
     </div>
-  )
+  );
 }
